b155af78f1fe01f378d3a21e15cbe024d035255b
Pixel <84857474+git-pixel22@users.noreply.github.com>
2024-02-02 19:55:00 +0530

    removed logs

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 // Listen for messages from content script
-console.log("Starting background.js")
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // Check if the submission was accepted
     if (request.action === "acceptedSubmission") {
